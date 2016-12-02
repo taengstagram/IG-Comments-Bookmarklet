21533884695213ec6c186b990f985dc6f7bf2fdb
taengstagram <taengstagram@gmail.com>
2016-12-02 17:07:48 +0800

    Fix code format

--- load.js
+++ load.js
@@ -1,17 +1,17 @@
 (function () {
     "use strict";
 
-    const latest_file = 'https://raw.githubusercontent.com/taengstagram/IG-Comments-Bookmarklet/master/latest.txt';
+    const latestFile = 'https://raw.githubusercontent.com/taengstagram/IG-Comments-Bookmarklet/master/latest.txt';
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", latest_file, true);
+    xhr.open("GET", latestFile, true);
     xhr.onreadystatechange = function() { 
         if (xhr.readyState != 4) {
             return;
         }
-        let bookmarklet_src = xhr.responseText;
+        let bookmarkletSrc = xhr.responseText;
         let head = document.getElementsByTagName('head')[0];
         let scriptBookmarklet = document.createElement('script');
-        scriptBookmarklet.src = bookmarklet_src;
+        scriptBookmarklet.src = bookmarkletSrc;
         scriptBookmarklet.async = false;
         head.appendChild(scriptBookmarklet);
     };
